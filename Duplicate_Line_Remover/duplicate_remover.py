d8e53da672a56cef99d6df217e2df12f7ba100d8
nullsc <53406164+nullsc@users.noreply.github.com>
2020-07-29 01:02:58 +0100

    Create duplicate_remover.py

---
+++ Duplicate_Line_Remover/duplicate_remover.py
@@ -0,0 +1,25 @@
+#duplicate_remover.py
+#Set the output and input files 
+#Set the delimiter to the part between the email and password
+#Once run the python file will remove any duplicate email lines
+#and write the new list to the output file
+
+print("Email Duplicate Remover")
+
+DELIMITER = ":"
+OUTPUT = "output.txt" #this will write a new file
+INPUT = "list.txt"
+
+emails_seen = set() # holds the full line
+with open(OUTPUT, "w") as output_file:
+    for each_line in open(INPUT, "r"):
+        if DELIMITER in each_line: #check if the line contains the delimiter
+            email = each_line.split(DELIMITER)[0] #split into email & password
+            passw = each_line.strip().split(DELIMITER)[1]
+            # print(email)
+            
+            if email not in emails_seen: # check if only email is a duplicate
+                output_file.write(email + DELIMITER + passw + "\n") #write the whole new line
+                emails_seen.add(email) #add email to the set
+
+            
