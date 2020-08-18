98f5cd4b1f78af293f100945e6a3b3ccc8577b71
nullsc <53406164+nullsc@users.noreply.github.com>
2020-08-18 21:59:36 +0100

    Update shortener.cpp

--- Line-Shortener/shortener.cpp
+++ Line-Shortener/shortener.cpp
@@ -4,21 +4,24 @@
 #include <fstream>
 
 
-size_t CharNum = 6; //must be > 0
+size_t CharNum = 0; 
 std::string inputFile = "list.txt";
 std::string outputFile = "output.txt";
-bool verbose = false; //debug
+bool verbose = true;
 
 void log(std::ofstream& infile, std::string& text);
+int shortenLeft(std::string& filename, std::string& logfile, size_t number);
+int shortenRight(std::string& filename, std::string& logfile, size_t number); //input, output, number of chars to remove
+
 
 int main()
 {
-    std::cout << "Line Remover\n";
-	std::cout << "Version 1.0\n\n";
+        std::cout << "Line Remover\n";
+	std::cout << "Version 1.1\n\n";
 
 	std::cout << "Input file: " << inputFile << "\n";
 	std::cout << "Output file: " << outputFile << "\n\n";
-	std::cout << "How many characters do you want to remove?\n";
+	std::cout << "How many character do you want to remove?\n";
 
 	std::cin >> CharNum;
 	if (CharNum < 1) {
@@ -26,34 +29,14 @@ int main()
 		return EXIT_FAILURE;
 	}
 
+
 	std::cout << "Removing the first " << CharNum << " characters" "\n\n";
+	
+	shortenRight(inputFile, outputFile, CharNum);
 
-	std::ifstream infile(inputFile);
-	std::string line;
 
-	if (!infile) {
-		std::cout << "Error: can't open " << inputFile << "\n";
-		return EXIT_FAILURE;
-	}
-
-	std::ofstream outfile(outputFile, std::ofstream::app); //append mode
-	if (!outfile) {
-		std::cout << "Error: can't open " << outputFile << "\n";
-		return EXIT_FAILURE;
-	}
-
-	while (infile >> line) {
-		//std::cout << line + "\n"; /*print all lines in file*/
-		if (line.size() > CharNum) {
-			//if (verbose) std::cout << line + "\n"; /*print all lines in file*/
-			std::string newline = line.substr(CharNum, line.size());
-			if (verbose) std::cout << newline + "\n"; /*shortened*/
-			log(outfile, newline);
-		}
-	}
-
-	outfile.close();
-
+	getchar();
+	return EXIT_SUCCESS;
 
 }
 
@@ -66,3 +49,57 @@ void log(std::ofstream& outfile, std::string& text) { //pass by reference
 	}
 }
 
+int shortenLeft(std::string& filename, std::string& logfile, size_t number) {
+	std::ifstream infile(filename);
+	std::string line;
+
+	if (!infile) {
+		std::cout << "Error: can't open " << inputFile << "\n";
+		return EXIT_FAILURE;
+	}
+
+	std::ofstream outfile(logfile, std::ofstream::app); //append mode
+	if (!outfile) {
+		std::cout << "Error: can't open " << logfile << "\n";
+		return EXIT_FAILURE;
+	}
+
+	while (infile >> line) {
+		if (line.size() > number) {
+			std::string newline = line.substr(number, line.size()); //remove from the right
+			if (verbose) std::cout << newline + "\n"; /*shortened*/
+			log(outfile, newline);
+		}
+	}
+
+	outfile.close();
+	return EXIT_SUCCESS;
+}
+
+int shortenRight(std::string& filename, std::string& logfile, size_t number) {
+	std::ifstream infile(filename);
+	std::string line;
+
+	if (!infile) {
+		std::cout << "Error: can't open " << inputFile << "\n";
+		return EXIT_FAILURE;
+	}
+
+	std::ofstream outfile(logfile, std::ofstream::app); //append mode
+	if (!outfile) {
+		std::cout << "Error: can't open " << logfile << "\n";
+		return EXIT_FAILURE;
+	}
+
+	while (infile >> line) {
+		if (line.size() > number) {
+			//if (verbose) std::cout << line + "\n"; /*print all lines in file*/
+			std::string newline = line.substr(0, line.size() - number); //remove from the right
+			if (verbose) std::cout << newline + "\n"; /*shortened*/
+			log(outfile, newline);
+		}
+	}
+
+	outfile.close();
+	return EXIT_SUCCESS;
+}
