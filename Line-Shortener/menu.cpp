ca6c4b283cf32ee8392986e2143665c4874e2517
nullsc <53406164+nullsc@users.noreply.github.com>
2020-09-11 00:06:04 +0100

    Update menu.cpp

--- Line-Shortener/menu.cpp
+++ Line-Shortener/menu.cpp
@@ -9,26 +9,30 @@ void banner() {
 
 void printMenu() {
 	std::cout << "-------------- Menu --------------\n";
-	std::cout << "[1] Shorten from the left \n";
-	std::cout << "[2] Shorten from the right \n";
+	std::cout << "[1] Shorten from the left [number of chars to remove]\n";
+	std::cout << "[2] Shorten from the right [number of chars to remove]\n";
 	std::cout << "[3] Delete before [string/char] \n";
 	std::cout << "[4] Delete after [string/char] \n";
+	std::cout << "[5] Delete all blank lines \n";
+	std::cout << "[6] Trimmer [left/right/both]\n";
+	std::cout << "[7] Remove duplicate lines\n";
+	std::cout << "[8] Line append [left/right/both] \n";
 	std::cout << "[0] Exit \n";
 	std::cout << "----------------------------------\n";
 }
 
-std::string &rtrim(std::string& str) { //trim trailing whitespace from the right - log always adds a \n
+std::string& rtrim(std::string& str) { //trim trailing whitespace from the right - works but log always adds a \n
 	size_t space = str.find_last_not_of(whitespace);
 	if (space != std::string::npos)
 		str.erase(space + 1);
 	else
-		str.clear();
+		str.clear(); //all whitespace
 
 	return str;
 
 }
 
-std::string& ltrim(std::string& str) { //trim trailing whitespace from the left
+std::string& ltrim(std::string& str) { //trim trailing whitespace from the left - works
 	size_t space = str.find_first_not_of(whitespace); //find the first non w/space char
 	if (space != std::string::npos) {
 		str = str.substr(space, str.size());
@@ -40,3 +44,6 @@ std::string& ltrim(std::string& str) { //trim trailing whitespace from the left
 std::string& trim(std::string& str) {
 	return ltrim(rtrim(str));
 }
+
+
+//maybe make inline to make it faster
