9ef5e1acc8054b37f32087adf528eb854050ca64
nullsc <53406164+nullsc@users.noreply.github.com>
2020-09-11 00:21:47 +0100

    Update menu.h

--- Line-Shortener/menu.h
+++ Line-Shortener/menu.h
@@ -10,11 +10,5 @@
 void banner();
 void printMenu();
 
-const std::string whitespace = " \t\f\v\n\r"; //needs to be const
-
-std::string& rtrim(std::string &str);
-std::string& ltrim(std::string& str);
-std::string& trim(std::string& str);
-
 
 #endif
