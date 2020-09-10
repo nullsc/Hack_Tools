c6cc830da54962415c17eda118126d1ad83fc464
nullsc <53406164+nullsc@users.noreply.github.com>
2020-09-11 00:18:54 +0100

    Add files via upload

--- Line-Shortener/functions.h
+++ Line-Shortener/functions.h
@@ -11,10 +11,19 @@
 #define RIGHT 2
 #define BOTH 3
 
-#define MAX_VECTOR_SIZE 1536870912  // ~0.5GB
+#define MAX_VECTOR_SIZE 1536870912  //10000 0.5GB
 
-void log(std::ofstream& outfile, std::string& text); //
+const std::string whitespace = " \t\f\v\n\r"; //needs to be const
+
+void log(std::ofstream& outfile, std::string& text); //was ofstream& infile
+bool isAllBlank(std::string& str);
 unsigned long getListSize(std::ifstream* inFile); //file needs to be already opened
 int removeDuplicates(std::ifstream* inFile, std::ofstream* logFile);
 
+
+std::string& rtrim(std::string& str);
+std::string& ltrim(std::string& str);
+std::string& trim(std::string& str);
+
+int lineAppend(std::ifstream* inFile, std::ofstream* logFile, std::string& str, size_t mode);
 #endif
